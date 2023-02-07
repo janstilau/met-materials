fdc17e965cd32a43665bd1a37b4d409476666d92
liuguoqiang <janstilau@gmail.com>
2023-02-07 20:20:23 +0800

    mod: sync

--- 23-animation/projects/final/Animation/Shared/Game/GameController.swift
+++ 23-animation/projects/final/Animation/Shared/Game/GameController.swift
@@ -33,34 +33,34 @@
 import MetalKit
 
 class GameController: NSObject {
-  var scene: GameScene
-  var renderer: Renderer
-  var options = Options()
-  var deltaTime: Double = 0
-  var lastTime: Double = CFAbsoluteTimeGetCurrent()
-  static var fps: Double = 0
-
-  init(metalView: MTKView, options: Options) {
-    Self.fps = Double(metalView.preferredFramesPerSecond)
-    renderer = Renderer(metalView: metalView, options: options)
-    scene = GameScene()
-    super.init()
-    self.options = options
-    metalView.delegate = self
-  }
+    var scene: GameScene
+    var renderer: Renderer
+    var options = Options()
+    var deltaTime: Double = 0
+    var lastTime: Double = CFAbsoluteTimeGetCurrent()
+    static var fps: Double = 0
+    
+    init(metalView: MTKView, options: Options) {
+        Self.fps = Double(metalView.preferredFramesPerSecond)
+        renderer = Renderer(metalView: metalView, options: options)
+        scene = GameScene()
+        super.init()
+        self.options = options
+        metalView.delegate = self
+    }
 }
 
 extension GameController: MTKViewDelegate {
-  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-    scene.update(size: size)
-    renderer.mtkView(view, drawableSizeWillChange: size)
-  }
-
-  func draw(in view: MTKView) {
-    let currentTime = CFAbsoluteTimeGetCurrent()
-    let deltaTime = (currentTime - lastTime)
-    lastTime = currentTime
-    scene.update(deltaTime: Float(deltaTime))
-    renderer.draw(scene: scene, in: view)
-  }
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        scene.update(size: size)
+        renderer.mtkView(view, drawableSizeWillChange: size)
+    }
+    
+    func draw(in view: MTKView) {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        let deltaTime = (currentTime - lastTime)
+        lastTime = currentTime
+        scene.update(deltaTime: Float(deltaTime))
+        renderer.draw(scene: scene, in: view)
+    }
 }
